4a0f44601f311f5550283d561c7c9f33b2cabb94
Savranin-Denis <savranindenisv@gmail.com>
2026-09-07 10:04:23 +0300

    lifting understanding

--- src/components/ClickCounter/ClickCounter.tsx
+++ src/components/ClickCounter/ClickCounter.tsx
@@ -1,20 +1,8 @@
-// import { useState } from "react";
-
-// export default function ClickCounter() {
-//   const [clicks, setClicks] = useState(0);
-
-//   const handleClick = () => {
-//     setClicks(clicks + 1);
-//   };
-
-//   return <button onClick={handleClick}>Clicked: {clicks}</button>;
-// }
-
 interface ClickCounterProps {
   value: number;
   onUpdate: () => void;
 }
 
 export default function ClickCounter({ value, onUpdate }: ClickCounterProps) {
-  return <button onClick={onUpdate}>Clicked: {value}</button>;
+  return <button onClick={onUpdate}>CLicked: {value}</button>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-// import "./index.css";
-import App from "./components/App/App";
 import "modern-normalize";
 import "./global.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
