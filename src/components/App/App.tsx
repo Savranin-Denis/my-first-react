6d308c5433db2e4ec9018e9f1cce890ccd02cdc5
Savranin-Denis <savranindenisv@gmail.com>
2026-09-07 17:25:25 +0300

    lifting another example

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,37 +1,84 @@
-import ClickCounter from "../ClickCounter/ClickCounter";
+// import ClickCounter from "../ClickCounter/ClickCounter";
+// import { useState } from "react";
+
 import { useState } from "react";
+import ClickCounter from "../ClickCounter/ClickCounter";
+
+// export default function App() {
+//   const [clicksA, setClicksA] = useState(0);
+//   const [сlicksB, setClicksB] = useState(0);
+
+//   // const handleClicksA = () => {
+//   //   setClicksA(clicksA + 1);
+//   // };
+//   // const handleClicksB = () => {
+//   //   setClicksB(сlicksB + 1);
+//   // };
+
+//   const handleUpdate = (type: "a" | "b") => {
+//     if (type === "a") {
+//       setClicksA(clicksA + 1);
+//     } else {
+//       setClicksB(сlicksB + 1);
+//     }
+//   };
+
+//   const handleReset = () => {
+//     setClicksA(0);
+//     setClicksB(0);
+//   };
+
+//   const totalClicks = clicksA + сlicksB;
+//   return (
+//     <>
+//       <ClickCounter value={clicksA} onUpdate={() => handleUpdate("a")} />
+//       <ClickCounter value={сlicksB} onUpdate={() => handleUpdate("b")} />
+//       <p>Total: {totalClicks}</p>
+//       {totalClicks > 0 && <button onClick={handleReset}>Reset all</button>}
+//     </>
+//   );
+// }
+
+// export default function App() {
+//   const [value, setValue] = useState(false);
+
+//   const handleClick = () => {
+//     setValue(!value);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={handleClick}>Hide p</button>
+//       <hr />
+//       {value && <p>Hello world</p>}
+//     </>
+//   );
+// }
+
+interface Obj {
+  a: number;
+  b: number;
+  c: number;
+}
 
 export default function App() {
-  const [clicksA, setClicksA] = useState(0);
-  const [сlicksB, setClicksB] = useState(0);
+  const [click, setClick] = useState({ a: 0, b: 0, c: 0 });
 
-  // const handleClicksA = () => {
-  //   setClicksA(clicksA + 1);
-  // };
-  // const handleClicksB = () => {
-  //   setClicksB(сlicksB + 1);
-  // };
-
-  const handleUpdate = (type: "a" | "b") => {
-    if (type === "a") {
-      setClicksA(clicksA + 1);
-    } else {
-      setClicksB(сlicksB + 1);
-    }
+  const handleClick = (key: keyof Obj) => {
+    setClick((prevValue) => ({ ...prevValue, [key]: prevValue[key] + 1 }));
   };
 
-  const handleReset = () => {
-    setClicksA(0);
-    setClicksB(0);
-  };
+  const total = click.a + click.b + click.c;
 
-  const totalClicks = clicksA + сlicksB;
   return (
     <>
-      <ClickCounter value={clicksA} onUpdate={() => handleUpdate("a")} />
-      <ClickCounter value={сlicksB} onUpdate={() => handleUpdate("b")} />
-      <p>Total: {totalClicks}</p>
-      {totalClicks > 0 && <button onClick={handleReset}>Reset all</button>}
+      <ClickCounter onUpdate={() => handleClick("a")} value={click.a} />
+      <hr />
+      <ClickCounter onUpdate={() => handleClick("b")} value={click.b} />
+      <hr />
+      <ClickCounter onUpdate={() => handleClick("c")} value={click.c} />
+      <hr />
+      <p>Total is: {total}</p>
     </>
   );
 }
--- src/components/ClickCounter/ClickCounter.tsx
+++ src/components/ClickCounter/ClickCounter.tsx
@@ -1,8 +1,17 @@
+// interface ClickCounterProps {
+//   value: number;
+//   onUpdate: () => void;
+// }
+
+// export default function ClickCounter({ value, onUpdate }: ClickCounterProps) {
+//   return <button onClick={onUpdate}>CLicked: {value}</button>;
+// }
+
 interface ClickCounterProps {
   value: number;
   onUpdate: () => void;
 }
 
 export default function ClickCounter({ value, onUpdate }: ClickCounterProps) {
-  return <button onClick={onUpdate}>CLicked: {value}</button>;
+  return <button onClick={onUpdate}>Cliked: {value}</button>;
 }
